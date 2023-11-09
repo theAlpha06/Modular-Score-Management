3de27be14d8716b9d4961124502a365c84c49b23
Shubham <shubhamsinghmau2@gmail.com>
2023-11-10 02:04:00 +0530

    setup instruction

---
+++ README.md
@@ -0,0 +1,25 @@
+# [Setup Instructions](https://github.com/theAlpha06/Modular-Score-Management)
+
+>Follow these instructions to set up the Modular Player Score Management System on your local machine using Node.js and Docker.
+
+## Prerequisites
+
+- Node.js and npm installed on your system.
+- Docker installed on your system.
+
+## Setup Steps
+
+### 1. Clone the Repository
+
+```bash
+git clone https://github.com/theAlpha06/Modular-Score-Management
+```
+
+```bash
+cd modularPlayerScoreManagement
+```
+
+### 2. Build and run Docker Images
+```bash
+docker-compose up --build
+```
--- controllers/playerController.js
+++ controllers/playerController.js
@@ -1,16 +1,79 @@
 import Player from '../models/Player.js';
 
-const createPlayer = async (req, res) => {};
+const createPlayer = async (req, res) => {
+  try {
+    const { name, country, score } = req.body;
+    const player = new Player({ name, country, score });
+    await player.save();
+    res.json(player);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-const updatePlayer = async (req, res) => {};
+const updatePlayer = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, score } = req.body;
+    const player = await Player.findByIdAndUpdate(id, { name, score }, { new: true });
+    if (!player) {
+      return res.status(404).json({ error: 'Player not found' });
+    }
+    res.json(player);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-const deletePlayer = async (req, res) => {};
+const deletePlayer = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const player = await Player.findByIdAndDelete(id);
+    if (!player) {
+      return res.status(404).json({ error: 'Player not found' });
+    }
+    res.json({ message: 'Player deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-const getAllPlayers = async (req, res) => {};
+const getAllPlayers = async (req, res) => {
+  try {
+    const players = await Player.find().sort({ score: 'desc' });
+    res.json(players);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-const getPlayerByRank = async (req, res) => {};
+const getPlayerByRank = async (req, res) => {
+  try {
+    const { val } = req.params;
+    const rank = parseInt(val);
+    const player = await Player.findOne().sort({ score: 'desc' }).skip(rank - 1);
+    if (!player) {
+      return res.status(404).json({ error: 'Player not found' });
+    }
+    res.json(player);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-const getRandomPlayer = async (req, res) => {};
+const getRandomPlayer = async (req, res) => {
+  try {
+    const count = await Player.countDocuments();
+    const random = Math.floor(Math.random() * count);
+    const player = await Player.findOne().skip(random);
+    if (!player) {
+      return res.status(404).json({ error: 'Player not found' });
+    }
+    res.json(player);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 export {
   createPlayer,
